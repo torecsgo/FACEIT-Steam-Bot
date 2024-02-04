60d99aac5bbe947227d770522982beee210ff530
Marcos <52284432+torecsgo@users.noreply.github.com>
2024-02-04 14:34:02 +0100

    Add files via upload

---
+++ faceit_utils/faceit.js
@@ -0,0 +1,82 @@
+const axios = require("axios");
+const FACEIT_KEY = require("../config.json").FACEIT_KEY;
+const toreSteamId = require("../config.json").toreSteamId;
+
+// Game INFO
+async function obtenerInfo(matchId) {
+    const url = `https://open.faceit.com/data/v4/matches/${matchId}`;
+    const headers = {
+        'accept': 'application/json',
+        'Authorization': FACEIT_KEY
+    };
+    try {
+        const response = await axios.get(url, { headers });
+        let server = await response.data.voting.location.pick[0];
+        let map = await response.data.voting.map.pick[0];
+        let info = [server, map];
+        return info;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function obtenerId(req) {
+    let matchId = req.body.payload.id;
+    return matchId;
+}
+
+function obtenerIp(req) {
+    let serverIp = req.body.payload.client_custom.server_ip;
+    let serverPort = req.body.payload.client_custom.server_port;
+    let ip = `${serverIp}:${serverPort}`
+    return ip;
+}
+
+// ELO
+async function obtenerProbabilidad(matchId) {
+    const url = `https://open.faceit.com/data/v4/matches/${matchId}`;
+    const headers = {
+        'accept': 'application/json',
+        'Authorization': FACEIT_KEY
+    };
+    try {
+        const response = await axios.get(url, { headers });
+        let winProbability;
+        for (let i = 0; i < response.data.teams.length; ++i) {
+            for (let j = 0; j < response.data.teams[i].roster.length; ++j) {
+                let playerId = response.data.teams[i].roster[j].game_player_id;
+                if (playerId === toreSteamId) {
+                    winProbability = response.data.teams[i].stats.winProbability;
+                    // Break out of the loop once the player is found
+                    break;
+                }
+            }
+            // Break out of the outer loop if the player is found
+            if (winProbability !== undefined) {
+                break;
+            }
+        }
+        return winProbability;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+async function calcularElo(matchId) {
+    let winProbability = await obtenerProbabilidad(matchId);
+    winProbability = Math.max(0, Math.min(1, winProbability));
+    let puntuacion = Math.floor((winProbability - 0.5) / 0.02) + 25;
+    return puntuacion;
+}
+
+
+
+
+module.exports = {
+    obtenerInfo: obtenerInfo,
+    obtenerId: obtenerId,
+    obtenerIp: obtenerIp,
+    calcularElo: calcularElo
+};
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require("./config.json");
+
+const faceit_utils = require("./faceit_utils/faceit");
+
+const toreSteamId = config.toreSteamId;
+const steamLogOnOptions = config.steamLogOnOptions;
+const partidas = config.partidas;
+
+const app = express();
+const port = 3000;
+
+const SteamUser = require('steam-user');
+const client = new SteamUser();
+
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Inicio STEAM BOT y Config
+client.logOn(steamLogOnOptions);
+client.on('loggedOn', async() => {
+    console.log('Logged into Steam with -> ' + steamLogOnOptions.accountName);
+    client.setPersona(SteamUser.EPersonaState.Online);
+    client.gamesPlayed(730);
+});
+
+// POST endpoint to receive webhooks
+app.post('/webhook', (req, res) => {
+    let tipoObtenido = req.body.event;
+    switch(tipoObtenido) {
+        case partidas.READY:
+            partidaReady(req);
+            break;
+        case partidas.FINALIZADO:
+            break;
+        case partidas.CREADO:
+            break;
+    }
+    res.status(200).send('Webhook received successfully');
+});
+
+async function partidaReady(req) {
+    let ip = faceit_utils.obtenerIp(req);
+    let matchId = faceit_utils.obtenerId(req);
+    let info = await faceit_utils.obtenerInfo(matchId);  // Agrega await aquí
+    let eloWin = await faceit_utils.calcularElo(matchId);
+    let server = info[0];
+    let map = info[1];
+    let date = new Date();
+    let formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}-${date.getHours()}:${date.getMinutes()}`;
+    await client.chat.sendFriendMessage(toreSteamId,
+    `Game ready: ${formattedDate}
+    -> Lobby: https://www.faceit.com/en/cs2/room/${matchId}
+    -> Server: ${server}
+    -> Map: ${map}
+    -> connect ${ip}
+    -> ELO Win: +${eloWin}; ELO Lose: -${Math.abs(50 - eloWin)}`);
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
